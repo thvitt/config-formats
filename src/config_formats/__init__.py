3a27e82476d6bd48d29c63a6a9a143038ebab80e
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2025-04-06 16:44:38 +0200

    read implementation

--- src/config_formats/__init__.py
+++ src/config_formats/__init__.py
@@ -1,9 +1,17 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from numbers import Number
 from typing import IO, Any, Type, ClassVar, Iterable, Mapping
-from zipfile import Path
+from pathlib import Path
 import io
 import sys
+from cyclopts import App
+from rich.pretty import pprint
+import logging
+from rich.logging import RichHandler
+
+logger = logging.getLogger(__name__)
+
+app = App()
 
 
 def dumb_down(data: Any) -> list | dict | str | int | float | bool | None:
@@ -38,7 +46,7 @@ def dumb_down(data: Any) -> list | dict | str | int | float | bool | None:
     return str(data)
 
 
-class Format(ABCMeta):
+class Format(ABC):
     name: ClassVar[str]
     suffixes: ClassVar[list[str]]
     label: ClassVar[str]
@@ -54,9 +62,11 @@ class Format(ABCMeta):
     def read(self) -> Any:
         if self.path is None:
             return self.load(sys.stdin.buffer)
-        else:
+        elif hasattr(self.path, "open"):
             with self.path.open("rb") as f:
                 return self.load(f)
+        else:
+            return self.load(self.path)
 
     def write(self, data: Any) -> None:
         if self.path is None:
@@ -95,12 +105,81 @@ class TOML(Format):
 
     def load(self, stream: IO[bytes]) -> Any:
         import tomllib
-        return tomllib.load(stream)
+
+        data = tomllib.load(stream)
+        if len(data) == 1 and "DEFAULT" in data:
+            data = data["DEFAULT"]
+        return data
 
     def dump(self, data: Any, stream: IO[bytes]) -> None:
         import tomli_w
-        tomli_w.dump(dumb_down(data), f)
+
+        processed_data = dumb_down(data)
+        if not isinstance(processed_data, Mapping):
+            processed_data = {"DEFAULT": processed_data}
+        tomli_w.dump(processed_data, stream)
 
 
-def main() -> None:
-    print("Hello from config-formats!")
+def format_for(src: Path) -> str | None:
+    for format in Format.registry.values():
+        if src.suffix in format.suffixes:
+            return format.name
+
+
+def autodetect_read(src: Path | None) -> tuple[Any, Format]:
+    if src is None:
+        src_data = io.BytesIO(sys.stdin.buffer.read())
+        src_data.seek(0)
+    else:
+        src_data = None
+    for format_type in Format.registry.values():
+        try:
+            format = format_type(src or src_data)
+            return format, format.read()
+        except Exception as e:
+            logger.debug("%s is not a %s: %s", src or "stdin", format_type.name, e)
+            if src_data:
+                src_data.seek(0)
+    raise ValueError("No format was able to read the source")
+
+
+@app.default
+def convert(
+    src: Path | None = None,
+    dst: Path | None = None,
+    /,
+    *,
+    from_: str | None = None,
+    to_: str | None = None,
+):
+    """
+    Convert between configuration formats.
+
+    Args:
+        src: file to read from. If missing, use standard input.
+        dst: file to write to. If missing, use standard output.
+        from_: source format.
+        to_: destination format.
+    """
+    logging.basicConfig(
+        level=logging.DEBUG,
+        handlers=[RichHandler(show_time=False)],
+        format="%(message)s",
+    )
+
+    if from_ is None and src is not None:
+        from_ = format_for(src)
+    if to_ is None and dst is not None:
+        to_ = format_for(dst)
+
+    if from_ is None:
+        src_format, data = autodetect_read(src)
+    elif from_ in Format.registry:
+        src_format = Format.registry[from_](src)
+        data = src_format.read()
+    else:
+        raise ValueError(
+            f"Unknown source format: {from_}. Must be one of {', '.join(Format.registry)}"
+        )
+
+    pprint(data)
